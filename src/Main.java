791e1495f2274dd9cbf0c657db1c389a2103e891
Alex <loalantref@hmail.com>
2022-12-10 15:29:20 +1200

    Вторая часть

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String completeName = lastName + firstName + middleName;
+        System.out.println(completeName);
+        System.out.println("=============================  Задание 2 ==================================");
+
+        String completeName2 = completeName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + completeName2);
     }
 }
